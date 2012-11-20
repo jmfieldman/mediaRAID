5276bd8b3659a72157104924642e4716e8e3664a
Jason Fieldman <jason@fieldman.org>
2012-11-19 19:46:41 -0800

    added notification stub for C component

---
+++ src/notif.c
@@ -0,0 +1,13 @@
+//
+//  notif.c
+//  mediaRAID
+//
+//  Created by Jason Fieldman on 11/19/12.
+//  Copyright (c) 2012 Jason Fieldman. All rights reserved.
+//
+
+#include <stdio.h>
+
+void mediaRAID_post_notification(const char *notification, const char *parameter) {
+	/* This is a stub */
+}
